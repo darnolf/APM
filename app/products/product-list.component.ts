c83d3803c3723ce8d642d4b56930943879aa1300
Daniel Arnolf <daniela@spiralsolutions.co.il>
2017-02-21 15:46:01 +0200

    Data binding and pipes

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
     {
         "productId": 1,
@@ -60,4 +64,8 @@ export class ProductListComponent {
     }
 ];
 
- }
+toggleImage(): void{
+    this.showImage = !this.showImage;
+}
+
+}
