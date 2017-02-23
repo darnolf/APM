85433d31d1d15e867dd91f88d4ac7afd47866774
Daniel Arnolf <daniela@spiralsolutions.co.il>
2017-02-23 10:46:12 +0200

    Final with Detail Pages Routing, Product Module

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ProductService } from './products/product.service';
 
 @Component({
     selector: 'pm-app',
@@ -18,12 +17,10 @@ import { ProductService } from './products/product.service';
             <router-outlet></router-outlet>
         </div>
      </div>
-    `,
-    providers: [ProductService]
+    `
 })
 export class AppComponent {
     pageTitle: string = "Acme Product Manager"
  }
 
- // stam
  
